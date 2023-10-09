4ea77d5f5fc0a6a05070cf5d7f09b29fbca9b58c
Varun Taneja <varunt298@gmail.com>
2023-10-09 08:58:52 +0530

    Animated UI

--- My project/Assets/Scripts/TextureUI.cs
+++ My project/Assets/Scripts/TextureUI.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
 
 public class TextureUI : MonoBehaviour
 {
     [SerializeField] private RectTransform panelRectTransform;
+
+    [Header("LeanTween")]
+    [SerializeField] private RectTransform scrollArea;
+    [SerializeField] private RectTransform leftButton;
+    [SerializeField] private RectTransform RightButton;
+    [SerializeField] private RectTransform crossButton;
+
+    [Header("Values")]
     [SerializeField] private float buttonScrollAmount = 20f;
     [SerializeField] private float buttonScrollLerpDuration = 0.2f;
 
@@ -17,7 +24,6 @@ public class TextureUI : MonoBehaviour
     private void Start()
     {
         ChangeModelPartTexture.OnPartSelected += EnableUI;
-        gameObject.SetActive(false);
     }
 
 
@@ -29,14 +35,36 @@ public class TextureUI : MonoBehaviour
 
     private void EnableUI(ModelPart part)
     {
-        gameObject.SetActive(true);
+        //tweening
+        float tweenTime = 1f;
+        LeanTween.move(scrollArea, new Vector2(0f, 100f), tweenTime).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(scrollArea, new Vector2(0f, 50f), tweenTime / 3f).setDelay(tweenTime).setEase(LeanTweenType.easeInCubic);
+        LeanTween.move(leftButton, new Vector2(60f, 100f), tweenTime).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.move(leftButton, new Vector2(30f, 100f), tweenTime / 3f).setDelay(tweenTime).setEase(LeanTweenType.easeInCubic);
+        LeanTween.move(RightButton, new Vector2(-60f, 100f), tweenTime).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.move(RightButton, new Vector2(-30f, 100f), tweenTime / 3f).setDelay(tweenTime).setEase(LeanTweenType.easeInCubic);
+        LeanTween.move(crossButton, new Vector2(80f, -50f), tweenTime).setEase(LeanTweenType.easeInOutCubic);
+        LeanTween.move(crossButton, new Vector2(50f, -50f), tweenTime / 3f).setDelay(tweenTime).setEase(LeanTweenType.easeInCubic);
+
+
         currentPart = part;
     }
 
 
     public void DisableUI()
     {
-        gameObject.SetActive(false);
+        //tweening
+        float tweenTime = 1f;
+        LeanTween.move(scrollArea, new Vector2(0f, 100f), tweenTime / 3f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(scrollArea, new Vector2(0f, -280f), tweenTime).setDelay(tweenTime /3f).setEase(LeanTweenType.easeInCubic); 
+        LeanTween.move(leftButton, new Vector2(60f, 100f), tweenTime / 3f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(leftButton, new Vector2(-130f, 100f), tweenTime).setDelay(tweenTime /3f).setEase(LeanTweenType.easeInCubic);        
+        LeanTween.move(RightButton, new Vector2(-60f, 100f), tweenTime / 3f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(RightButton, new Vector2(130f, 100f), tweenTime).setDelay(tweenTime /3f).setEase(LeanTweenType.easeInCubic);
+        LeanTween.move(crossButton, new Vector2(80f, -50f), tweenTime / 3f).setEase(LeanTweenType.easeOutCubic);
+        LeanTween.move(crossButton, new Vector2(-130f, -50f), tweenTime).setDelay(tweenTime /3f).setEase(LeanTweenType.easeInCubic);
+
+
         currentPart.DeselectPart();
         OnPartDeselect?.Invoke();
         panelRectTransform.anchoredPosition = Vector2.zero;   //resetting position of scroll rect
@@ -47,7 +75,8 @@ public class TextureUI : MonoBehaviour
     {
         float scrollRectWidth = panelRectTransform.parent.GetComponent<RectTransform>().rect.width;
 
-        if(Mathf.Abs(panelRectTransform.anchoredPosition.x - buttonScrollAmount) < panelRectTransform.rect.width - scrollRectWidth)
+        //to prevent scrolling if already at the edge
+        if (Mathf.Abs(panelRectTransform.anchoredPosition.x - buttonScrollAmount) < panelRectTransform.rect.width - scrollRectWidth)
         {
             StartCoroutine(ButtonScrollLerp(panelRectTransform, -1, buttonScrollAmount));
         }
@@ -61,6 +90,7 @@ public class TextureUI : MonoBehaviour
     
     public void LeftScrollButton()
     {
+        //to prevent scrolling if already at the edge
         if(panelRectTransform.anchoredPosition.x + buttonScrollAmount < 0)
         {
             StartCoroutine(ButtonScrollLerp(panelRectTransform, 1, buttonScrollAmount));
